024d8bad1f49a2f1d74ad9b617f3757777945e29
Ariqfadh <161322240+Ariqfadh@users.noreply.github.com>
2024-12-09 23:31:05 +0700

    Update app.py

--- backend-git/backend/app/app.py
+++ backend-git/backend/app/app.py
@@ -23,11 +23,9 @@ app.add_middleware(
 onnx_model_path = "blood_cells_v8.onnx"
 session = ort.InferenceSession(onnx_model_path)
 
-
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
 
 
 # Preprocessing function sesuai kebutuhan model ONNX
@@ -59,13 +57,13 @@ async def get_prediction(file: UploadFile = File(...)):
         class_id = np.argmax(class_probabilities)
         
         # Daftar nama kelas
-        class_names = ['EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL','BASOPHIL']  # Sesuaikan dengan dataset Anda
+        class_names = ['Basophil', 'Eosinophil', 'Lymphocyte', 'Monocyte', 'Neutrophil']  # Sesuaikan dengan dataset Anda
         
         # Tentukan threshold confidence (misalnya 0.5 atau 50%)
         CONFIDENCE_THRESHOLD = 0.4
         
         if max_confidence < CONFIDENCE_THRESHOLD:
-            predicted_label = "Undetected"
+            predicted_label = str(max_confidence)
         else:
             predicted_label = class_names[class_id] if class_id < len(class_names) else "Unknown"
         
